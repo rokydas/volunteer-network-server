fa9e00418031396b2642a199ea1998b31c1d867d
rokydas <droky136@gmail.com>
2020-10-05 01:44:50 +0600

    registeredEvent api created

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const user = process.env.USER;
 const userPassword = process.env.USER_PASSWORD;
 const dbName = process.env.DB_NAME;
-const eventCollection = 'event'; 
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${user}:${userPassword}@cluster0.vetwi.mongodb.net/${dbName}?retryWrites=true&w=majority`;
@@ -22,25 +21,39 @@ const uri = `mongodb+srv://${user}:${userPassword}@cluster0.vetwi.mongodb.net/${
 const client = new MongoClient(uri, { useUnifiedTopology: true}, { useNewUrlParser: true });
 
 client.connect(err => {
-    const collection = client.db(dbName).collection(eventCollection);
-    console.log(user, userPassword, dbName, eventCollection)
+    const eventCollection = client.db(dbName).collection('event');
+    const RegisteredEventCollection = client.db(dbName).collection('registeredEvent');
 
-    // app.post('/addEvent', (req, res) => {
-    //     const event = req.body;
-    //     console.log(event)
-    //     collection.insertMany(event)
-    //     .then(result => {
-    //         console.log(result);
-    //         res.send(result.insertedCount)
-    //     })
-    // })
+    app.post('/addEvent', (req, res) => {
+        const event = req.body;
+        eventCollection.insertMany(event)
+        .then(result => {
+            res.send(result.insertedCount)
+        })
+    })
+
+    app.post('/addRegisteredEvent', (req, res) => {
+        const registeredEvent = req.body;
+        // console.log(registeredEvent);
+        RegisteredEventCollection.insertOne(registeredEvent)
+        .then(result => {
+            res.send(result.insertedCount)
+        })
+    })
 
     app.get('/', (req, res) => {
         res.send("Hey, It is created by Roky Vhai.");
     })
     
     app.get('/events', (req, res) => {
-        collection.find({})
+        eventCollection.find({})
+        .toArray((err, documents) => {
+            res.send(documents);
+        })
+    })
+
+    app.get('/registeredEvent', (req, res) => {
+        RegisteredEventCollection.find({})
         .toArray((err, documents) => {
             res.send(documents);
         })
