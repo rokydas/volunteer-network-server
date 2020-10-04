20f2ee0b5e6c80391f684a2742bb852413b700da
rokydas <droky136@gmail.com>
2020-10-05 00:26:31 +0600

    problem solved 80% sure

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express'); 
 const bodyParser = require('body-parser'); 
 const cors = require('cors');
+require('dotenv').config()
 
 const app = express(); 
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-require('dotenv').config()
+
 
 const user = process.env.USER;
 const userPassword = process.env.USER_PASSWORD;
